841977ad5f6b7b4f5581e61f0f9b0199035f08c5
RichH <RichH@beacon-optical.co.uk>
2018-05-17 11:56:21 +0100

    Scans Organized and Chaotic sessions

--- viconConvert/surveyEnfs.py
+++ viconConvert/surveyEnfs.py
@@ -16,29 +16,61 @@ import glob
 import os
 import ConfigParser
 
+
 class ShootingDay( object ):
     """ Object representing a shooting day """
-    def __init__( self, rate, multiplier ):
+    
+    def __init__( self, rate, multiplier, source_dir ):
+        assert( source_dir is not None )
         self.rate = rate
         self.multiplier = multiplier
-        self.source_dir = ""
-        self.encoutered_subjects = []
-        self.encoutered_calibrations = []
-        self.subject_mixes = Set()
+        self.source_dir = source_dir
+        self.encountered_subjects = set()
+        self.encountered_calibrations = {}
+        self.subject_mixes = set()
         self.takes = {}
+        self.sessions = []
+        self._cur_session = ""
         self.remarks = ""
         
         
     def newTake( self, name ):
         take = Take( self.rate, self.multiplier )
         take.name = name
-        self.takes[ name ] = take
+        while( take.name in self.takes ):
+            take.name += "_"
+        self.takes[ take.name ] = take
+        take.session = self._cur_session
         return take
+
+
+    def updateWith( self, take ):
+        """ Examine takes calibration and subject list """
+        assert( take in self.takes.values() )
+        # Scan the take for session info
+        calibration_fq = os.path.join( self.source_dir, take.session, take.calibration_file )
+        # calibration
+        # TODO: fingerprinting file byu sz works in 98% case, should really read, for P mats, then hash them
+        f_stat = os.stat( calibration_fq )
+        cal_sz = f_stat.st_size
+        if( cal_sz not in self.encountered_calibrations ):
+            self.encountered_calibrations[ cal_sz ] = []
+        self.encountered_calibrations[ cal_sz ].append( take.calibration_file )
         
+        # subject list
+        subs = take.subject_list
+        if( subs in self.subject_mixes ):
+            return
         
-    def updateWith( self, take_name ):
-        """ Examine takes calibration and subject list"""
-        pass
+        self.subject_mixes.add( subs )
+        for sub in subs:
+            self.encountered_subjects.add( sub )
+
+        
+    def newSession( self, session ):
+        self.sessions.append( session )
+        self._cur_session = session
+
         
         
 class Take( tc.TcRange ):
@@ -55,19 +87,20 @@ class Take( tc.TcRange ):
         self.name = "NAME"
         self._creation_time = "CREATIONDATEANDTIME"
         self.subject_list = []
-        self.calibration_name = ""
+        self.calibration_file = ""
         self.notes = "NOTES"
         self.description = "DESCRIPTION"
+        self.session = ""
         
         
         
-def getSessionTakes( path ):
-    res = glob.glob( os.path.join( path, "*.enf" ) )
+def getSessionTakes( day_path, session, shoot ):
+    res = glob.glob( os.path.join( day_path, session, "*.enf" ) )
     takes = [ t for t in res if ".Take" in t ]
 
     cf = ConfigParser.SafeConfigParser( Take._DEFAULTS )
 
-    shoot = ShootingDay( 25, 4 )
+    shoot.newSession( session )
     for t in takes:
 
         cf.read( t )
@@ -75,32 +108,41 @@ def getSessionTakes( path ):
         star = cf.get( "TRIAL_INFO", "STARTTIMECODE" )
         durr = cf.get( "TRIAL_INFO", "DURATIONFIELDS" )
         date = cf.get( "TRIAL_INFO", "CREATIONDATEANDTIME" )
-        note = cf.get( "TRIAL_INFO", "NOTES" )
-        desc = cf.get( "TRIAL_INFO", "DESCRIPTION" )
         cal  = cf.get( "TRIAL_INFO", "CAMERACALIBRATION" )
         subs = cf.get( "TRIAL_INFO", "SUBJECTS" )
+        note = cf.get( "TRIAL_INFO", "NOTES" )
+        desc = cf.get( "TRIAL_INFO", "DESCRIPTION" )
+        
         
         take = shoot.newTake( name )
-        
-        take.setFromStartDur( star, durr )
-        
         take._file_fq  = t
         take.file_name = os.path.basename( t )
-        
+        take.setFromStartDur( star, durr )
         take._creation_time = date
-        
+        take.calibration_file = cal
+        take.subject_list = tuple( sorted( subs.split( "," ) ) )
         take.notes = note
         take.description = desc
         
-        take.subject_list = subs.split( "," )
+        shoot.updateWith( take )
+        
 
-        shoot.updateWith( name )
-    return shoot
-    
-# test
-path = r"C:\ViconData\Teaching\ShootingDays\180226_A1_GroupB_01\PM"
-d = getSessionTakes( path )
+def findSessions( path ):
+    """ At a 'Capture Day' level, we're looking for dirs containing 'dir_name.Session???.enf' """
+    ess = [] # Eclipse Subsessions
+    for d in os.listdir( path ):
+        res = glob.glob( os.path.join( path, d, "*Session*.enf" ) )
+        if( len( res ) > 0 ):
+            ses_name = os.path.basename( os.path.dirname( res[0] ) )
+            ess.append( ses_name )
+    return ess
+        
+# test 1
+path = r"C:\ViconData\Teaching\ShootingDays\170323_A1_MosCap01"
+day_sessions = findSessions( path )
+shoot = ShootingDay( 25, 4, path )
+for ses in day_sessions:
+    getSessionTakes( path, ses, shoot )
 
-print d[0].file_name
-print d[0].subject_list
-print d[0]
+print shoot.encountered_subjects
+print shoot.takes.keys()
